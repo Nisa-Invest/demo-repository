116fc598554e383ab033f652091133a35838280d
diddydeeable <diddydeeable@gmail.com>
2024-12-21 18:49:41 +0000

    route and template of homepage

---
+++ nisa-invest/src/pages/HomePage.tsx
@@ -0,0 +1,24 @@
+import { SimpleCard } from "../components/Card/SimpleCard";
+export function HomePage() {
+  return (
+    <div>
+      <div className="grid grid-cols-6 gap-4">
+        <div className="col-start-2 col-span-2 pt-20">
+          {" "}
+          <SimpleCard title={"Book a meeting with your financial advisor!"} />
+        </div>
+        <div className="col-start-4 col-span-2 ...">
+          <SimpleCard title={"See your dashboard"} />
+        </div>
+        
+        <div className="col-start-2 col-span-4 ...">
+          {" "}
+          <SimpleCard title={"FAQs"} />
+        </div>
+        <div className="col-start-4 col-span-2 ...">
+          <SimpleCard title={"My Notes"} />
+        </div>
+      </div>
+    </div>
+  );
+};
--- nisa-invest/src/routes/Routes.tsx
+++ nisa-invest/src/routes/Routes.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { LandingPage } from "../pages/LandingPage.tsx";
 import { DashboardPage } from "../pages/DashboardPage";
 import { FinancialPlanningQuiz } from "../components/Quiz/Quiz.tsx";
+import { HomePage } from "../pages/HomePage.tsx"
 
 export function RouterRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element = {<HomePage />} />
       <Route path="/quiz" element={<FinancialPlanningQuiz />} />
       <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
